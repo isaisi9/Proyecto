01e192dfd83306e3918e3fec12fc293d25a54dba
kzollers <65082448+kzollers@users.noreply.github.com>
2020-05-21 12:12:09 -0500

    Update chart-area-demo.js
    ok

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Ene", "Feb", "Mar", "Abr", "May"],
     datasets: [{
-      label: "Earnings",
+      label: "Dato",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 5000, 7000, 8100, 16000, 20000],
+      data: [0, 7000, 17500, 18300, 19100, 20000],
     }],
   },
   options: {
@@ -110,7 +110,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + 'Donadores ' ;
         }
       }
     }
